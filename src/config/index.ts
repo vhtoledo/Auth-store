01c8ec621737251be88a747267d766f9eaefe8b0
victor toledo <querynetworking@gmail.com>
2023-12-06 23:33:45 -0300

    Creacion de usuario, manejo de errores, generar jwt

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
 
 export * from './envs'
 export * from './regular-exp'
+export * from './bcrypt.adapter'
+export * from './jwt.adapter'
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
+import { AuthService } from '../services/auth.service';
 
 
 
@@ -10,8 +11,9 @@ export class AuthRoutes {
   static get routes(): Router {
 
     const router = Router();
+    const authService = new AuthService();
 
-    const controller = new AuthController();
+    const controller = new AuthController(authService);
     
     // Definir las rutas
     router.post('/login', controller.loginUser );
