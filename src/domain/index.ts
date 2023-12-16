643c8ed0eec3e8e6263a93de8ffaca12559bdac5
victor toledo <querynetworking@gmail.com>
2023-12-16 01:55:58 -0300

    creacion de pagination DTO

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -7,3 +7,5 @@ export * from './dtos/auth/register-user.dto';
 export * from './dtos/auth/login-user.dto';
 
 export * from './dtos/empleado/create-empleado.dto';
+
+export * from './dtos/shared/pagination.dto';
