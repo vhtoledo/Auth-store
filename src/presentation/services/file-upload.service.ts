e8abcd114949df1298445215f65cb77ab72cbbd8
victor toledo <querynetworking@gmail.com>
2023-12-21 00:16:59 -0300

    Cambiar nombre al archivo y validar

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,11 +1,15 @@
 import path from 'path';
 import fs from 'fs';
 import { UploadedFile } from "express-fileupload";
+import { Uuid } from '../../config';
+import { CustomError } from '../../domain';
 
 
 export class FileUploadService {
 
-    constructor() {}
+    constructor(
+        private readonly uuid = Uuid.v4
+    ) {}
 
     private checkFolder( folderPath: string ) {
         if( !fs.existsSync(folderPath) ) {
@@ -21,15 +25,27 @@ export class FileUploadService {
 
         try {
 
-          const fileExtension = file.mimetype.split('/').at(1);
+          const fileExtension = file.mimetype.split('/').at(1) ?? '';
+          if ( !validEntensions.includes(fileExtension) ) {
+            throw CustomError
+                .badRequest(`Invalid extension: ${ fileExtension }, valid ones ${ validEntensions }`);
+          }
+
+
+
           const destination = path.resolve( __dirname, '../../../', folder);
           this.checkFolder( destination );
 
-          file.mv(destination + `/mi-imagen.${ fileExtension}`);
+          const fileName = `${ this.uuid() }.${ fileExtension }`;
+
+          file.mv(`${destination}/${ fileName}`);
+
+          return { fileName };
             
         } catch (error) {
             
-            console.log({error})
+            console.log({error});
+            throw error;
         }
 
 
